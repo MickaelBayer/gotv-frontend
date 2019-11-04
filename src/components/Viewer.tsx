71d21fd09e02791be08d76b9da032e3eaf8bfd75
Yoann TERUEL <yoann.teruel@gmail.com>
2019-11-04 09:13:12 +0100

    Fix import de fichier non présents

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "./Header";
-import SignOut from "../views/authentication/SignOut";
 import { Route } from "react-router-dom";
 import Home from "../views/Home";
 
@@ -10,9 +9,6 @@ export default class Viewer extends React.Component {
       <div>
         <Header />
         <Route exact path='/' component={Home} />
-        <Route exact path='/series' component={SignOut} />
-        <Route exact path='/event' component={SignOut} />
-        <Route exact path='/signout' component={SignOut} />
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignIn from './views/authentication/SignIn';
-import SignOut from './views/authentication/SignOut';
 import SignUp from './views/authentication/SignUp';
 import App from './App';
 import Footer from './components/Footer';
@@ -14,7 +13,6 @@ ReactDOM.render(
     <Route path="/" exact component={App} />
     <Route path="/signin" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
-    <Route path="/signout" exact component={SignOut} />
     <Footer />
   </Router>,
   document.getElementById('root'));
