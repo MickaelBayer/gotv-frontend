a4dd64cd500b2355e85a70e03b1ba0636a06a0cf
audrey <nunes.audrey31@gmail.com>
2019-11-07 13:54:34 +0100

    [UPD] background color

---
+++ src/components/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import '../styles/components/contact.scss';
+import { Link } from 'react-router-dom';
+
+export default class Contact extends React.Component {
+  render() {
+    return (
+      <div className="contactPage">
+          PAGE CONTACT
+      </div>
+    );
+  }
+}
