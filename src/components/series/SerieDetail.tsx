681f848d5b9f6a5e22b078babf0e50ff4295cee4
Yoann TERUEL <yoann.teruel@gmail.com>
2019-11-18 23:22:29 +0100

    Amélioration Header de la page détail série

--- src/components/series/SerieDetail.tsx
+++ src/components/series/SerieDetail.tsx
@@ -8,51 +8,72 @@ import { ISerie } from "../../models/serie.model";
 import SerieService from "../../services/api/entities/serie.service";
 import { Chip } from "@material-ui/core";
 import { Rating } from '@material-ui/lab';
+import Carousel from "react-multi-carousel";
+import { url } from "inspector";
 
 const SerieDetail: React.FunctionComponent = () => {
-    let { see_id } = useParams();
-    const initialStateSerieVideo: ISerieVideo[] = [new SerieVideo("", "", "", "")];
-    const initialStateSerie: ISerie = { see_id: 1, see_name: "", see_tmdb_id: 1, see_poster_path: "", see_overview: "", see_backdrop_path: "", see_first_air_date: "", see_original_country: "", see_original_lang: "", see_categories: [] };
-    const [serieVideos, setSerieVideos] = useState(initialStateSerieVideo);
-    const [serie, setSerie] = useState(initialStateSerie);
+	let { see_id } = useParams();
+	const initialStateSerieVideo: ISerieVideo[] = [new SerieVideo("", "", "", "")];
+	const initialStateSerie: ISerie = { see_id: 1, see_name: "", see_tmdb_id: 1, see_poster_path: "", see_overview: "", see_backdrop_path: "", see_first_air_date: "", see_original_country: "", see_original_lang: "", see_categories: [] };
+	const [serieVideos, setSerieVideos] = useState(initialStateSerieVideo);
+	const [serie, setSerie] = useState(initialStateSerie);
 
-    async function fetchSerie() {
-        const serieService = new SerieService();
-        const serieVideoService = new SerieVideoService();
-        serieService.get(Number(see_id)).then(res => {
-            serieVideoService.getVideos(res.see_tmdb_id).then(res => {
-                console.log(res)
-                setSerieVideos(res);
-            })
-            setSerie(res);
-        })
-    }
+	const responsive = {
+		desktop: {
+			items: 1,
+			breakpoint: { max: 3000, min: 1024 }
+		}
+	};
 
-    useEffect(() => {
-        fetchSerie();
-    }, [])
+	async function fetchSerie() {
+		const serieService = new SerieService();
+		const serieVideoService = new SerieVideoService();
+		serieService.get(Number(see_id)).then(res => {
+			serieVideoService.getVideos(res.see_tmdb_id).then(res => {
+				console.log(res)
+				setSerieVideos(res);
+			})
+			setSerie(res);
+		})
+	}
 
-    return (
-        <Row>
-            <Col>
-                {serieVideos.map((serieVideo, i) => {
-                    return (
-                        <Iframe url={`http://www.youtube.com/embed/${serieVideo.key}?enablejsapi=1`} width="100%" height="340px" key={i} className="p-4 details-video" />
-                    )
-                })}
-            </Col>
-            <Col>
-                <h3 className="mt-3">{serie.see_name}</h3>
-                <div className="d-flex align-items-center my-3">
-                    <Rating value={5} readOnly className="mr-3" />
-                    {serie.see_categories.map((categorie, i) => {
-                        return (<Chip size="small" key={i} label={categorie.cae_label} className="details-chip" />)
-                    })}
-                </div>
-                <p>{serie.see_overview}</p>
-            </Col>
-        </Row>
-    );
+	useEffect(() => {
+		fetchSerie();
+	}, [])
+
+	return (
+		<React.Fragment>
+			<header className="overview-header" style={{ backgroundImage: `url(${serie.see_backdrop_path})` }}>
+				<Container>
+					<div className="header-player">
+						<Carousel responsive={responsive}>
+							{serieVideos.filter(serieVideo => serieVideo.type == "Trailer").map((serieVideo, i) => {
+								return (
+									<Iframe url={`http://www.youtube.com/embed/${serieVideo.key}?enablejsapi=1`} width="640px" height="560px" key={i} />
+								)
+							})}
+						</Carousel>
+					</div>
+					<h1 className="title">{serie.see_name}</h1>
+				</Container>
+			</header>
+			<Container>
+				<Row>
+					<Col>
+
+						<div className="d-flex align-items-center my-3">
+							<Rating value={5} readOnly className="mr-3" />
+							{serie.see_categories.map((categorie, i) => {
+								return (<Chip size="small" key={i} label={categorie.cae_label} className="details-chip" />)
+							})}
+						</div>
+						<p>{serie.see_overview}</p>
+					</Col>
+				</Row>
+				Espace commantaire
+		</Container>
+		</React.Fragment>
+	);
 }
 
 export default SerieDetail;
--- src/components/series/SeriesPage.tsx
+++ src/components/series/SeriesPage.tsx
@@ -6,52 +6,52 @@ import { CatSerie } from "../../models/catSerie.model";
 import CatSerieService from "../../services/api/entities/catSerie.service";
 
 export const SeriesPage: React.FunctionComponent = (props) => {
-    const initialState: CatSerie[] = [{ cae_id: 1, cae_id_tmdb: 1, cae_label: "", cae_series: [] }]
-    const [catSeries, setCatSeries] = useState(initialState);
+	const initialState: CatSerie[] = [{ cae_id: 1, cae_id_tmdb: 1, cae_label: "", cae_series: [] }]
+	const [catSeries, setCatSeries] = useState(initialState);
 
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-            slidesToSlide: 3, // optional, default to 1.
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-            slidesToSlide: 2, // optional, default to 1.
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1, // optional, default to 1.
-        },
-    };
+	const responsive = {
+		desktop: {
+			breakpoint: { max: 3000, min: 1024 },
+			items: 3,
+			slidesToSlide: 3, // optional, default to 1.
+		},
+		tablet: {
+			breakpoint: { max: 1024, min: 464 },
+			items: 2,
+			slidesToSlide: 2, // optional, default to 1.
+		},
+		mobile: {
+			breakpoint: { max: 464, min: 0 },
+			items: 1,
+			slidesToSlide: 1, // optional, default to 1.
+		},
+	};
 
-    async function fetchCatSeries() {
-        const catSerieService = new CatSerieService();
-        catSerieService.getAll().then(res => {
-            setCatSeries(res);
-        });
-    }
+	async function fetchCatSeries() {
+		const catSerieService = new CatSerieService();
+		catSerieService.getAll().then(res => {
+			setCatSeries(res);
+		});
+	}
 
-    useEffect(() => { fetchCatSeries() }, [])
+	useEffect(() => { fetchCatSeries() }, [])
 
-    return (
-        <div>
-            {catSeries.map((category, i) => {
-                return (
-                    <React.Fragment key={i}>
-                        <h1>{category.cae_label}</h1>
-                        <Carousel responsive={responsive}>
-                            {category.cae_series.map((serie, i) => {
-                                return (
-                                    <SeriesCard serie={serie} key={i} />
-                                )
-                            })}
-                        </Carousel>
-                    </React.Fragment>
-                )
-            })}
-        </div>
-    );
+	return (
+		<div className="container d-block">
+			{catSeries.filter(catserie => catserie.cae_series.length > 0).map((category, i) => {
+				return (
+					<React.Fragment key={i}>
+						<h1>{category.cae_label}</h1>
+						<Carousel responsive={responsive}>
+							{category.cae_series.map((serie, i) => {
+								return (
+									<SeriesCard serie={serie} key={i} />
+								)
+							})}
+						</Carousel>
+					</React.Fragment>
+				)
+			})}
+		</div>
+	);
 }
