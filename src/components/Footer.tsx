1388412b0779eb9ba7927289fe0f9c6fede2812f
mickael <mickael.bayer@agreenculture.fr>
2019-11-20 09:56:24 +0100

    [UPD] Switch Container App

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import "../styles/components/footer.scss";
 import {Container, Row, Col } from 'react-bootstrap/';
+import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 const Footer: React.FunctionComponent = () => {
+
+    let location = useLocation().pathname.slice(1);
+    console.log(location)
     return (
         <div className="foot">
-            <div id="triangle-footer-bottomright"></div>
-            <div className="foot-div">
+            <div id={"triangle-footer-" + location}></div>
+            <div className={"foot-div-" + location}>
                 <Container className="remove-all-margin-padding">
                     <Row className="remove-all-margin-padding">
                         <Col className="remove-all-margin-padding">
-                            Qui sommes nous ?
+                            <NavLink to="/" className="linkNav">Qui sommes nous ?</NavLink>
                         </Col>
                         <Col className="remove-all-margin-padding">
-                            Nous contacter
+                            <NavLink to="/contact" className="linkNav">Nous contacter</NavLink>
                         </Col>
                         <Col className="remove-all-margin-padding">
-                            Mentions légales
+                            <NavLink to="/" className="linkNav">Mentions légales</NavLink>
                         </Col>
                     </Row>
                     <Row className="remove-all-margin-padding">
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -36,7 +36,7 @@ const map = new DOMParser().parseFromString(hexagones, 'text/xml').getElementsBy
 console.log(map);
 
 export default class Home extends React.Component {
-    
+
 
     render() {
         return (
@@ -47,22 +47,22 @@ export default class Home extends React.Component {
                         <div className="winner">
                             <img src={winner} alt="Top 10" width="300" className="responsive"></img>
                         </div>
-                        <br/>
+                        <br />
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit sed ipsum non finibus. Nulla lacinia sapien mollis turpis condimentum volutpat. Morbi finibus lectus est, eu ultrices leo molestie et. Donec orci ante, ornare eu neque vel, volutpat ultricies risus. Ut eleifend sem in volutpat dapibus. Curabitur vel ultrices tellus, at tristique augue. Nunc ornare mi lorem, eget molestie tellus volutpat consectetur. Cras condimentum, ex finibus tincidunt suscipit, nisl justo dignissim nisl, sit amet condimentum diam elit sagittis sapien. Aenean pulvinar ante dui, sollicitudin sollicitudin ligula interdum a. Proin quis urna ac sem rhoncus rutrum. Sed aliquam ex dui, eget dignissim mi mattis quis. In tortor velit, finibus nec dictum in, sollicitudin sit amet metus. Morbi a tellus accumsan nisi pharetra pulvinar ac in lectus. Quisque enim lacus, iaculis sed venenatis ut, facilisis in arcu. Aliquam eleifend orci a tortor ultricies ullamcorper.
                         </p>
-                        <br/>
+                        <br />
                     </div>
                     <div className="top10_2" id="hexa">
-                        <br/>
+                        <br />
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit sed ipsum non finibus. Nulla lacinia sapien mollis turpis condimentum volutpat. Morbi finibus lectus est, eu ultrices leo molestie et. Donec orci ante, ornare eu neque vel, volutpat ultricies risus. Ut eleifend sem in volutpat dapibus. Curabitur vel ultrices tellus, at tristique augue. Nunc ornare mi lorem, eget molestie tellus volutpat consectetur. Cras condimentum, ex finibus tincidunt suscipit, nisl justo dignissim nisl, sit amet condimentum diam elit sagittis sapien. Aenean pulvinar ante dui, sollicitudin sollicitudin ligula interdum a. Proin quis urna ac sem rhoncus rutrum. Sed aliquam ex dui, eget dignissim mi mattis quis. In tortor velit, finibus nec dictum in, sollicitudin sit amet metus. Morbi a tellus accumsan nisi pharetra pulvinar ac in lectus. Quisque enim lacus, iaculis sed venenatis ut, facilisis in arcu. Aliquam eleifend orci a tortor ultricies ullamcorper.
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
                     </div>
                     <div className="expert-triangle-top"></div>
                     <div className="expert_panel">
-                        <img src={experts} id="experts-img" alt="L'expertise en caoutchouc !" width="300" className="responsive"></img> 
-                        <br/>
+                        <img src={experts} id="experts-img" alt="L'expertise en caoutchouc !" width="300" className="responsive"></img>
+                        <br />
                         <span id="experts">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit sed ipsum non finibus. Nulla lacinia sapien mollis turpis condimentum volutpat. Morbi finibus lectus est, eu ultrices leo molestie et. Donec orci ante, ornare eu neque vel, volutpat ultricies risus. Ut eleifend sem in volutpat dapibus. Curabitur vel ultrices tellus, at tristique augue. Nunc ornare mi lorem, eget molestie tellus volutpat consectetur. Cras condimentum, ex finibus tincidunt suscipit, nisl justo dignissim nisl, sit amet condimentum diam elit sagittis sapien. Aenean pulvinar ante dui, sollicitudin sollicitudin ligula interdum a. Proin quis urna ac sem rhoncus rutrum. Sed aliquam ex dui, eget dignissim mi mattis quis. In tortor velit, finibus nec dictum in, sollicitudin sit amet metus. Morbi a tellus accumsan nisi pharetra pulvinar ac in lectus. Quisque enim lacus, iaculis sed venenatis ut, facilisis in arcu. Aliquam eleifend orci a tortor ultricies ullamcorper.
                         </span>
