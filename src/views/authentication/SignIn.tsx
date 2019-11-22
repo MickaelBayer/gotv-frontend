ce0f8e10b9c26e482ae096b5c4c9484667d6ab14
Yoann TERUEL <yoann.teruel@gmail.fr>
2019-11-22 14:21:41 +0100

    Refactoring de l'appel du store et de la récup des data du form

--- src/views/authentication/SignIn.tsx
+++ src/views/authentication/SignIn.tsx
@@ -1,61 +1,69 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Cookie from 'js-cookie';
-import { AuthenticationService } from '../../services/api/authentication.service';
-import { Redirect } from 'react-router';
 import '../../styles/views/signin.scss';
 import logo from '../../assets/logo.png';
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { login } from '../../store/actions/auth.action';
+import { AuthActionTypes } from '../../store/types/auth.type';
 
-export default class SignIn extends Component {
-  // private authenticationService: AuthenticationService;
-  // state = { redirection: false };
-  // constructor(props: React.Component) {
-  //   super(props);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  //   this.authenticationService = new AuthenticationService();
-  // }
+const mapDispatchToProps = (dispatch: Dispatch<AuthActionTypes>) => ({
+  login: bindActionCreators(login, dispatch)
+})
 
-  // handleSubmit = (event: any) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.target);
-  //   this.authenticationService
-  //     .login({ usr_pseudo: data.get('pseudo'), password: data.get('password') })
-  //     .then(response => {
-  //       if (response.status === 200 && response.data.token) {
-  //         let jwt = response.data.token;
-  //         Cookie.set('token', jwt);
-  //         this.setState({ redirection: true });
-  //       } else if (response.status === 500) {
-  //       }
-  //     });
-  // };
+type Props = ReturnType<typeof mapDispatchToProps>;
+
+class SignIn extends React.Component<Props, { usr_pseudo: string, password: string }> {
+
+  constructor (props: Readonly<Props>) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangePseudo = this.handleChangePseudo.bind(this);
+    this.state = {
+      usr_pseudo: "",
+      password: ""
+    }
+  }
+
+  public handleChangePseudo(event: any) {
+    this.setState({ usr_pseudo: event.target.value });
+  }
+
+  public handleChangePassword(event: any) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit = async (event: any) => {
+    event.preventDefault();
+    this.props.login(this.state.usr_pseudo, this.state.password);
+  }
 
   render() {
-    // const { redirection } = this.state;
-    // if (redirection) {
-    //   //Affichage de la redirection
-    //   return <Redirect to="/" />;
-    // }
     return (
       <div className="signin">
         <div className="logoSignin">
           <img src={logo} width={110} alt="logo" />
         </div>
         <div className="titleConnexion"> CONNEXION </div>
-        <Form className="formSignin">
-          <Form.Group controlId="pseudo">
+        <Form className="formSignin" onSubmit={this.handleSubmit}>
+          <Form.Group controlId="usr_pseudo">
             <Form.Label>Pseudo:</Form.Label>
-            <Form.Control type="text" placeholder="Pseudo" name="pseudo" />
+            <Form.Control
+              type="text"
+              placeholder="Pseudo"
+              value={this.state.usr_pseudo}
+              onChange={this.handleChangePseudo} />
           </Form.Group>
           <Form.Group controlId="password">
             <Form.Label>Mot de passe:</Form.Label>
             <Form.Control
               type="password"
               placeholder="Mot de passe"
-              name="password"
-            />
+              value={this.state.password}
+              onChange={this.handleChangePassword} />
           </Form.Group>
           <div className="forgetPwd">
             <a href="/resetpwd">Mot de passe oublié ?</a>
@@ -70,3 +78,5 @@ export default class SignIn extends Component {
     );
   }
 }
+
+export default connect(null, mapDispatchToProps)(SignIn)
