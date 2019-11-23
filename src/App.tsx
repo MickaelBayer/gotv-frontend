ed27158e847b1300975cd3c36403e5691b294069
Yoann TERUEL <yoann.teruel@gmail.com>
2019-11-23 02:17:46 +0100

    Propagation des infos user dans toute l'appli + exemple avec SerieDetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Container } from 'react-bootstrap/';
+import React, { useEffect } from 'react';
+import { Container, Spinner } from 'react-bootstrap/';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Contact from './components/Contact';
@@ -13,28 +13,51 @@ import Account from './views/authenticated/Account';
 import ForgetPassword from './views/authentication/ForgetPassword';
 import SignIn from './views/authentication/SignIn';
 import SignUp from './views/authentication/SignUp';
+import { AuthenticationService } from './services/api/authentication.service';
+import { UserActionTypes } from './store/modules/user/user.type';
+import { bindActionCreators, Dispatch } from 'redux';
+import { getUserInfo } from './store/modules/user/user.action';
+import { connect } from 'react-redux';
+import { AppState } from './store';
 
+const mapStateToProps = (state: AppState) => ({
+  user: state.user.user,
+  isLoading: state.user.isLoading
+})
 
-const App: React.FunctionComponent = () => {
+const mapDispatchToProps = (dispatch: Dispatch<UserActionTypes>) => ({
+  getUserInfo: bindActionCreators(getUserInfo, dispatch)
+})
+
+type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+
+const App: React.FunctionComponent<Props> = (props) => {
+  useEffect(() => {
+    if (AuthenticationService.isAuth()) {
+      props.getUserInfo();
+    }
+  }, []);
+
   return (
     <Router>
       <Container>
         <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/series' component={SeriesPage} />
-          <Route path="/serie/:see_id" component={SerieDetail} />
-          <Route path='/signin' component={SignIn} />
-          <Route path='/signup' component={SignUp} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/resetpwd' component={ForgetPassword} />
-          <Route path='/account' component={Account} />
-          <Route component={PageNotFound} />
-        </Switch>
+        {props.isLoading && AuthenticationService.isAuth() ? <Spinner animation="border" /> :
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/series' component={SeriesPage} />
+            <Route path="/serie/:see_id" render={() => <SerieDetail user={props.user} />} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/resetpwd' component={ForgetPassword} />
+            <Route path='/account' component={Account} />
+            <Route component={PageNotFound} />
+          </Switch>}
         <Footer />
       </Container>
     </Router>
   )
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
