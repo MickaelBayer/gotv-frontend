2a1ae79e67539f81e37649da356715145017860f
audrey <nunes.audrey31@gmail.com>
2019-11-28 20:25:16 +0100

    [FIX] footer css + signin + signup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,39 @@
-import React from "react";
-import "../styles/components/footer.scss";
+import React from 'react';
+import '../styles/components/footer.scss';
 import { Container, Row, Col } from 'react-bootstrap/';
 import { NavLink } from 'react-router-dom';
 import { useLocation } from 'react-router';
 
 const Footer: React.FunctionComponent = () => {
-
-    let location = useLocation().pathname.slice(1);
-    console.log(location)
-    return (
-        <div className="footer">
-            <div id={"triangle-footer-" + location}></div>
-            <div className={"foot-div-" + location}>
-                <Container>
-                    <Row>
-                        <Col>
-                            <NavLink to="/" className="linkNav-foot">Qui sommes nous ?</NavLink>
-                        </Col>
-                        <Col>
-                            <NavLink to="/contact" className="linkNav-foot">Nous contacter</NavLink>
-                        </Col>
-                        <Col>
-                            <NavLink to="/" className="linkNav-foot">Mentions légales</NavLink>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            GoTV series © 2019 — Tous droits réservés ®
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </div>
-    )
-}
+  const location = useLocation().pathname.slice(1);
+  return (
+    <div className="footer">
+      <div className={'foot-div-' + location}>
+        <Container>
+          <Row>
+            <Col>
+              <NavLink to="/" className="linkNav-foot">
+                Qui sommes nous ?
+              </NavLink>
+            </Col>
+            <Col>
+              <NavLink to="/contact" className="linkNav-foot">
+                Nous contacter
+              </NavLink>
+            </Col>
+            <Col>
+              <NavLink to="/" className="linkNav-foot">
+                Mentions légales
+              </NavLink>
+            </Col>
+          </Row>
+          <Row>
+            <Col>GoTV series © 2019 — Tous droits réservés ®</Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
