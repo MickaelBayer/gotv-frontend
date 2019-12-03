f9fe128cd681ed8c04e276bfba70c76f11a11dbe
audrey <nunes.audrey31@gmail.com>
2019-12-03 11:28:09 +0100

    TEST

--- src/components/home/SearchPage.tsx
+++ src/components/home/SearchPage.tsx
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
-import "../../styles/components/searchPage.scss";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
+import React, { ChangeEvent, Component, useState } from 'react';
+import '../../styles/components/searchPage.scss';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import { AppState } from '../../store';
+import { bindActionCreators, Dispatch } from 'redux';
+import { SerieActionTypes } from '../../store/modules/serie/serie.type';
+import {
+  getAllSeries,
+  getBestSeries,
+  getSearchSeries
+} from '../../store/modules/serie/serie.action';
+import { ISerie } from '../../models/serie.model';
+import { connect } from 'react-redux';
 
-export default class SearchPage extends Component {
-reloadPage() {
-    window.location.reload()
-}
-  render() {
-    return (
-      <div className="searchPage">
-        <div className="closeBtn">
-            <div className="croix" onClick={this.reloadPage}>X</div>
-        </div>
-      <InputGroup className="searchInput">
-          <FormControl
-              className="personalizeInput"
-              placeholder="Rechercher"
-              aria-label="Username"
-              aria-describedby="basic-addon1"
-          />
-      </InputGroup>
-      </div>
-    );
+const mapStateToProps = (state: AppState) => ({
+  series: state.series.series,
+  isLoading: state.series.isLoading
+});
+
+const mapDispatchToProps = (dispatch: Dispatch<SerieActionTypes>) => ({
+  getSearchSeries: bindActionCreators(getSearchSeries, dispatch)
+});
+
+type Props = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+const SearchPage: React.FunctionComponent<Props> = props => {
+  function reloadPage() {
+    window.location.reload();
   }
-}
+  const [searchText, setValue] = useState({bob: ""});
+  const handleChangeSearchText = () => (event: ChangeEvent<any>) => {
+    event.preventDefault();
+    setValue({ bob: event.target.value.toString() });
+  };
+
+  return (
+    <div className="searchPage">
+      <div className="closeBtn">
+        <div className="croix" onClick={reloadPage}>
+          X
+        </div>
+      </div>
+      <InputGroup className="searchInput">
+        <FormControl
+          className="personalizeInput"
+          placeholder="Rechercher"
+          value={searchText}
+          onChange={handleChangeSearchText}
+        />
+      </InputGroup>
+    </div>
+  );
+};
+export default connect(mapStateToProps, mapDispatchToProps)(SearchPage);
